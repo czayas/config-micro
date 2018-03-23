11e180938152cf8dd1d0eaec92f0805e76438a8d
Carlos Augusto Zayas Guggiari <czayas@users.noreply.github.com>
2018-03-23 01:41:40 -0300

    Actualización a init.lua para guardar archivo antes de ejecutar.
    Al apretar F5, se guarda el archivo si fue modificado, y se ejecuta posteriormente.

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
 function python3()
     local buf = CurView().Buf -- El buffer ("archivo") actual
-    if buf:FileType() == "python" then
-        HandleShellCommand("python3 " .. buf.Path, true, true)
+    if buf:FileType() == "python" then -- Tal vez deba ser "python3"
+        if buf.IsModified then
+            buf:Save() -- Guarda el archivo antes de ejecutarlo
+        end
+        HandleShellCommand("python3 " .. buf.AbsPath, true, true)
         -- El primer true hace que no se ejecute en background
     end
 end
