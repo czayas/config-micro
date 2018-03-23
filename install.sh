50f4cb3b5a76c3cfa626aeeac26f5a9b839fa6e8
Carlos Zayas Guggiari <czayas@gmail.com>
2018-03-23 12:10:41 -0300

    Agregado script bash de instalación.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Instalador de config-micro - https://github.com/czayas/config-micro
+
+pushd ~/.config/micro/ >/dev/null || exit
+
+for archivo in bindings.json settings.json init.lua
+do
+    if [[ -e $archivo ]]
+    then
+        mv $archivo $archivo.backup
+    fi
+    wget https://raw.githubusercontent.com/czayas/config-micro/master/$archivo
+done
+
+popd >/dev/null || exit
