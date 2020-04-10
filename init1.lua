d2d67feb06200390a4f2b0e73e3f11acb54a7b5d
Carlos Zayas <czayas@gmail.com>
2020-04-10 04:28:37 -0400

    init1.lua

---
+++ init1.lua
@@ -0,0 +1,14 @@
+-- Para Micro Editor V1
+
+function python3()
+    local buf = CurView().Buf -- El buffer ("archivo") actual
+    if buf:FileType() == "python" then -- Tal vez deba ser "python3"
+        if buf.IsModified then
+            buf:Save() -- Guarda el archivo antes de ejecutarlo
+        end
+        HandleShellCommand("python3 " .. buf.AbsPath, true, true)
+        -- El primer true hace que no se ejecute en background
+    end
+end
+
+BindKey("F5", "init.python3")
