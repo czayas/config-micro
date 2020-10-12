cce7b8c00bf7fcb5cbb72dfb3c16483a709244c5
Carlos Augusto Zayas Guggiari <czayas@users.noreply.github.com>
2020-10-12 10:53:02 -0300

    Tratamiento de comillas en trayectorias de archivo con espacios.

--- init.lua
+++ init.lua
@@ -3,13 +3,17 @@
 local shell = import("micro/shell")
 local config = import("micro/config")
 
+function quote(str)
+    return '"'..str..'"'
+end
+
 function python3(bp)
     local buf = bp.Buf
     if buf:FileType() == "python" or buf:FileType() == "python3" then
         if buf:Modified() then
             bp:Save()
         end
-        shell.RunInteractiveShell("python3 " .. buf.AbsPath, true, false)
+        shell.RunInteractiveShell("python3 " .. quote(buf.AbsPath), true, false)
     end
 end
 
